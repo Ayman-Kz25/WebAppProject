08d4766f4362d1d58ebe21ef05e5bc964554a527
Ayman-Kz25 <kamalakz786@gmail.com>
2025-10-25 15:24:12 +0500

    Initial website setup

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('regForm').addEventListener('submit', function(e) {
+      e.preventDefault();
+      const name = document.getElementById('name').value.trim();
+      const email = document.getElementById('email').value.trim();
+      const dept = document.getElementById('dept').value.trim();
+      const msg = document.getElementById('msg');
+
+      if (name && email && dept) {
+        msg.innerText = "Registration Successful!";
+        msg.style.color = "#28a745";
+        msg.classList.add("show");
+        // this.reset();
+      } else {
+        msg.innerText = "⚠️ Please fill out all fields.";
+        msg.style.color = "#dc3545";
+        msg.classList.add("show");
+      }
+    });
